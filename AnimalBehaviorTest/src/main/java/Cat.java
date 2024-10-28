cb1f7961e72353abc8a6f2a05de4b5f53147f4a2
eEliash22 <163044135+eEliash22@users.noreply.github.com>
2024-10-27 20:22:14 -0600

    Add files via upload

---
+++ AnimalBehaviorTest/src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+    @Override
+    public void display() {
+        System.out.println("Cat is displayed");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat";
+    }
+}
---
+++ AnimalBehaviorTest/src/main/java/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+    @Override
+    public void display() {
+        System.out.println("Dog is displayed");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog";
+    }
+}
